4b9bb17532069fbb3ace9a9c7a078aa3140d8174
wannnhi <nanhi@gmail.com>
2024-08-13 00:39:02 +0900

    feat : scene1

--- Assets/01.Scripts/YWH/Stage1/JumpKeyWait.cs
+++ Assets/01.Scripts/YWH/Stage1/JumpKeyWait.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using System;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
 
 public class JumpKeyWait : MonoBehaviour
 {
     [SerializeField] GameObject scarecrow;
     [SerializeField] private CanvasGroup keyUI;
     [SerializeField] private InputReader InputReader;
+    [SerializeField] private PlayableDirector playableDirector;
     bool istouched;
-    private bool _isAttack=false;
+    private bool _isAttack = false;
+    private bool _isDead = false;
 
+   
 
     private void Start()
     {
         InputReader.OnAttackKeyEvent += HandleAttackKeyEvent;
     }
 
+    public void niulgull9()
+    {
+        _isDead = true;
+        Destroy(scarecrow);
+    } 
+
     private void HandleAttackKeyEvent()
     {
         _isAttack = true;
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -40,20 +52,30 @@ public class JumpKeyWait : MonoBehaviour
         ChatSystem.Instance.TypCoStart("스승님", "이제 익숙해진 것 같아 보이니..", 0.2f);
 
         yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
-
+        ChatSystem.Instance.StopTyp();
         ChatSystem.Instance.TypCoStart("스승님", "다음은 무술이다!", 0.2f);
         scarecrow.SetActive(true);
-
+        
         yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
-
+        ChatSystem.Instance.StopTyp();
         keyUI.gameObject.SetActive(true);
-        InputReader._controls.Enable();
+       
         keyUI.DOFade(1, 1);
-
-        yield return new WaitUntil(() => _isAttack==true);
+        InputReader._controls.Enable();
+        yield return new WaitUntil(() => _isAttack == true);
 
         keyUI.DOFade(0, 1).SetDelay(0.5f);
 
+        yield return new WaitUntil(() => _isDead == true);  
+
+        ChatSystem.Instance.TypCoStart("스승님", "이제 충분히 적응을 한 것 같아 보이니", 0.2f);
+        yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
+        ChatSystem.Instance.StopTyp();
+
+        ChatSystem.Instance.TypCoStart("스승님", "나를 따라와보거라!!", 0.2f);
+        yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
+        ChatSystem.Instance.StopTyp();
+        playableDirector.Play();
 
     }
 }
