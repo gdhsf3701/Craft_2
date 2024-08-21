311b40a711d01192f3fc55255ceb50ea27c9adcc
wannnhi <nanhi@gmail.com>
2024-08-21 19:17:46 +0900

    feat : stage1 finish

--- Assets/01.Scripts/YWH/Stage1/JumpKeyWait.cs
+++ Assets/01.Scripts/YWH/Stage1/JumpKeyWait.cs
@@ -48,7 +48,7 @@ public class JumpKeyWait : MonoBehaviour
 
     IEnumerator Musul()
     {
-        InputReader._controls.Disable();
+        PlayerManager.Instance.Player.PlayerInput._controls.LeftPlayer.Disable();
         ChatSystem.Instance.TypCoStart("스승님", "이제 익숙해진 것 같아 보이니..", 0.2f);
 
         yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
@@ -61,21 +61,48 @@ public class JumpKeyWait : MonoBehaviour
         keyUI.gameObject.SetActive(true);
        
         keyUI.DOFade(1, 1);
-        InputReader._controls.Enable();
+        Debug.Log("Enable");
+        PlayerManager.Instance.Player.PlayerInput._controls.LeftPlayer.Enable();
         yield return new WaitUntil(() => _isAttack == true);
 
         keyUI.DOFade(0, 1).SetDelay(0.5f);
 
-        yield return new WaitUntil(() => _isDead == true);  
-
+        yield return new WaitUntil(() => _isDead == true);
+        PlayerManager.Instance.Player.PlayerInput._controls.LeftPlayer.Disable();
         ChatSystem.Instance.TypCoStart("스승님", "이제 충분히 적응을 한 것 같아 보이니", 0.2f);
         yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
         ChatSystem.Instance.StopTyp();
+
+        FadeManager.instance.FadeIn(0.2f);
+        yield return new WaitForSecondsRealtime(2);
+        FadeManager.instance.FadeOut(0.2f);
 
         ChatSystem.Instance.TypCoStart("스승님", "나를 따라와보거라!!", 0.2f);
         yield return new WaitUntil(() => ChatSystem.Instance.endText == true);
         ChatSystem.Instance.StopTyp();
         playableDirector.Play();
+     
 
+
+    }
+
+    void OnEnable()
+    {
+        playableDirector.stopped += OnPlayableDirectorStopped;
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        if (playableDirector == aDirector)
+        {
+            PlayerManager.Instance.Player.PlayerInput._controls.LeftPlayer.Enable();
+            Debug.Log("Enable");
+
+        }
+
+    }
+    void OnDisable()
+    {
+        playableDirector.stopped -= OnPlayableDirectorStopped;
     }
 }
