93ea295bd42ced6be2953e28228488aee923fda3
moon080411 <anstjdalsdlek@gmail.com>
2024-09-11 18:41:41 +0900

    fix:endZipline

--- Assets/01.Scripts/MOON/Obstacle/Zipline.cs
+++ Assets/01.Scripts/MOON/Obstacle/Zipline.cs
@@ -28,6 +28,10 @@ public class Zipline : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         //text.gameObject.SetActive(false);
+        if (!isMove)
+        {
+            _player = null;
+        }
         _isPlayer = false;
     }
     private void Update()
@@ -42,7 +46,7 @@ public class Zipline : MonoBehaviour
                 _player.stateMachine.ChangeState(PlayerEnum.Wire);
             }
         }
-        if (isMove)
+        if (isMove&&_player != null)
         {
             _player.transform.position += (LineEnd.transform.position- _player.transform.position).normalized * Speed * Time.deltaTime;
             if(Mathf.Abs(Vector3.Distance(_player.transform.position, LineEnd.transform.position)) < 0.1f)
