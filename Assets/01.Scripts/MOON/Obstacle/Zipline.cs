ae2a7b1d03827208af8107692bf7f8c981910299
크시 <155462542+csiimnida@users.noreply.github.com>
2024-09-12 18:19:36 +0900

    Endme

--- Assets/01.Scripts/MOON/Obstacle/Zipline.cs
+++ Assets/01.Scripts/MOON/Obstacle/Zipline.cs
@@ -16,7 +16,7 @@ public class Zipline : MonoBehaviour
 
     SoundPlayer soundPlayer;
 
-    Player _player;
+    [SerializeField]Player _player;
 
     [SerializeField] float Speed=10;
     private void OnTriggerEnter2D(Collider2D collision)
