e6cbc57e2e63dcb80b6941664b9750798eac7598
moon080411 <anstjdalsdlek@gmail.com>
2024-10-08 16:49:36 +0900

    Feat:ReadonGame

--- Assets/01.Scripts/JES/Ui/GageGameUI.cs
+++ Assets/01.Scripts/JES/Ui/GageGameUI.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,35 +9,68 @@ public class GageGameUI : MonoBehaviour
 {
     private Tween _tween;
     private Image _barImage;
+    private  bool Fail = false;
+    [SerializeField]Stage3EnemyManager[] _enemyManagers;
+    GameObject nowEnemy;
+    bool done = false;
 
     private void Awake()
     {
         _barImage=transform.Find("Bar").GetComponent<Image>();
     }
-
+    private void OnEnable()
+    {
+        if (done && !Fail)
+        {
+            _tween = _barImage.DOFillAmount(1, 1f).OnComplete(() => FailSeq()).SetUpdate(true);
+            Time.timeScale = 0;
+        }
+        else if (Fail)
+        {
+            gameObject.SetActive(false);
+        }
+        done = true;
+    }
     private void Start()
     {
-        _tween = _barImage.DOFillAmount(1, 1f).OnComplete(() => FailSeq());
+        gameObject.SetActive(false);
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F)) {
+        if(Input.GetKeyDown(KeyCode.F)&&!Fail) {
             _tween.Kill();
             if(_barImage.fillAmount>=0.594f&& _barImage.fillAmount <= 0.736f)
                 ClearSeq();
             else
                 FailSeq();
         }
+    }   
+    public void ChangeNowEnemy(GameObject game)
+    {
+        nowEnemy = game;
     }
     private void ClearSeq()
     {
-        //실패했을때 함수
+        //성공했을때 함수
+        Destroy(nowEnemy);
+        _barImage.fillAmount = 0;
+        gameObject.SetActive(false);
+        //_barImage의 초기화
         print("성공");
     }
 
     private void FailSeq()
     {
+        Fail = false;
+        foreach(var enemy in _enemyManagers)
+        {
+            if (enemy.AllDeadCheck())
+            {
+                break;
+            }
+        }
         //실패했을때 함수
+        gameObject.SetActive(false);
         print("실패");
     }
 }
---
+++ Assets/01.Scripts/MOON/EnemyBackTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyBackTrigger : MonoBehaviour
+{
+    private GageGameUI gameUI;
+    private void Awake()
+    {
+        gameUI = GameObject.Find("GageBar").GetComponent<GageGameUI>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            gameUI.gameObject.SetActive(true);
+            gameUI.ChangeNowEnemy(gameObject);
+        }
+    }
+}
