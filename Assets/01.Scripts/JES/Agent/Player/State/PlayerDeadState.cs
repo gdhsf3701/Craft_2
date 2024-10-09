5b82494bcef0ede3183066fb8e69ef0ab79a009b
jes0321 <jesjes0321@naver.com>
2024-10-10 00:19:30 +0900

    feat : dead ui

--- Assets/01.Scripts/JES/Agent/Player/State/PlayerDeadState.cs
+++ Assets/01.Scripts/JES/Agent/Player/State/PlayerDeadState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class PlayerDeadState : PlayerState
 {
     public PlayerDeadState(Player player, PlayerStateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
@@ -8,6 +10,7 @@ public class PlayerDeadState : PlayerState
     {
         base.Enter();
         _player.MovementCompo.StopImmediately();
+        _player.gameObject.layer = LayerMask.NameToLayer("DeadBody");
     }
 
     public override void UpdateState()
